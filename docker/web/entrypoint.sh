a2512ffb8c31dbe50a88363561242ce47a5552fb
fuomag9 <1580624+fuomag9@users.noreply.github.com>
2025-12-28 20:19:46 +0100

    implemented rootless image and running

--- docker/web/entrypoint.sh
+++ docker/web/entrypoint.sh
@@ -6,7 +6,6 @@ DB_DIR=$(dirname "$DB_PATH")
 
 echo "Ensuring database directory exists..."
 mkdir -p "$DB_DIR"
-chown -R nextjs:nodejs "$DB_DIR"
 
 echo "Starting application..."
-exec gosu nextjs env HOSTNAME=0.0.0.0 node server.js
+exec env HOSTNAME=0.0.0.0 node server.js
